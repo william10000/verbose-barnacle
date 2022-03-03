c2a7b50112c0b69b6858306e2419f4aaae497acb
wwan <wwan@axio.com>
2022-03-02 20:00:02 -0500

    stopped here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { getTrials } from "./api/trial";
+import { Trial } from "./Trial";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const [userAdded, setUserAdded] = useState(false);
+  const [trials, setTrials] = useState([]);
+
+  useEffect(() => {
+    getTrials().then((trials) => {
+      console.log(trials);
+      // trials.forEach((trial) => {
+      //   trial.detailsVisible = true;
+      // });
+      setTrials(trials);
+    });
+  }, []);
+
+  console.log(trials);
+  const trialsDivs = trials.map((trial) => {
+    return <Trial {...trial} key={trial.id} />;
+  });
+
+  return <div className="App">{trialsDivs}</div>;
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("New user form is visible", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/add new user/i)).toBeVisible();
 });
